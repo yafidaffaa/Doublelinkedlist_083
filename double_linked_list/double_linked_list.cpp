adb3ff05258658ac738a585ea0e44b22ac391470
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-26 17:48:59 +0700

    kondisi else currentNode = start dan while currentNode not null

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -104,5 +104,12 @@ bool DoubleLinkedList::listEmpty() {
 void DoubleLinkedList::traverse() {
 	if (listEmpty())
 		cout << "\nList is empty" << endl;
-
+	else {
+		cout << "\nRecords in ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
 }

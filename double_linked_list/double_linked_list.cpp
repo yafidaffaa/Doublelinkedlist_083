5819a49da183a19352f2a6b34ca8b8a50a0db921
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-26 18:03:00 +0700

    method untuk hapus dengan kondisi if else

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -130,3 +130,17 @@ void DoubleLinkedList::revtraverse() {
 		}
 	}
 }
+
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nList is empty" << endl;
+	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number " << rollNo << " delete" << endl;
+}

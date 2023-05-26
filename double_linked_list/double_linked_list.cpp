d111463e2eb988170c82c0b08312b941b355acb3
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-26 17:56:54 +0700

    method revtraverse dengan kondisi if dan else didalamnya ada kondisi while

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -113,3 +113,20 @@ void DoubleLinkedList::traverse() {
 		}
 	}
 }
+
+void DoubleLinkedList::revtraverse() {
+	if (listEmpty()) {
+		cout << "\nList is empty" << endl;
+	}
+
+	else {
+		cout << "\nRecords in descanding order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
